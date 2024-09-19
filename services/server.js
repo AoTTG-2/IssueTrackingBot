8af814e3321deed43dd0bc5c8391e90fd0a9b0ac
AutumnThyme <79295722+AutumnThyme@users.noreply.github.com>
2024-09-18 23:13:46 -0400

    e
    e

--- services/server.js
+++ services/server.js
@@ -61,13 +61,14 @@ const createServer = client => {
           const thread = await channel.threads.fetch(threadId);
 
           // Get the applied tags
-          console.log(channel.appliedTags);
+          console.log(thread.parent.allowedTags);
+          console.log(thread.appliedTags);
 
           // Update the tag on discord to "In Progress"
           thread.setArchived(false);
           thread.setLocked(false);
           
-          const tags = thread.parent.allowedTags.filter(tag => tag.name !== "In Progress");
+          const tags = thread.parent.allowedTags.find(tag => tag.name !== "In Progress");
 
           if (tags.length > 0)
           {
