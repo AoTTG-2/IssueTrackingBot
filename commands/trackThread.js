680f70b8737ff701995987dcda430b0745301b65
AutumnThyme <79295722+AutumnThyme@users.noreply.github.com>
2024-09-19 00:16:46 -0400

    Update trackThread.js

--- commands/trackThread.js
+++ commands/trackThread.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits, ThreadChannel } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, ForumChannel } = require('discord.js');
 const {
     createGithubReference,
     parseGithubReference,
@@ -25,7 +25,7 @@ module.exports = {
         console.log(channel);
         
         // if the channel is not a forum, return an ephemeral error
-        if (typeof channel !== ThreadChannel) {
+        if (typeof channel !== ForumChannel) {
             await interaction.reply({content: 'The channel must be a thread.', ephemeral: true});
             return;
         }
