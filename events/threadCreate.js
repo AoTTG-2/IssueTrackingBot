ef75f0277cfb32e46d07998ecf7107dba6f8b51b
AutumnThyme <79295722+AutumnThyme@users.noreply.github.com>
2024-09-19 00:39:49 -0400

    Update threadCreate.js

--- events/threadCreate.js
+++ events/threadCreate.js
@@ -29,7 +29,8 @@ module.exports = {
 	name: Events.ThreadCreate,
 	async execute(thread, boolean) {
 		// check if channel is tracked
-		//if (!isChannelTracked(thread.parentId)) return;
+		console.log(thread.parentId);
+		if (!isChannelTracked(thread.parentId)) return;
 		await pairCreatedThreadWithIssue(thread);
 		// // Log the thread title, body, creator, and tags
 		// console.log(`A new thread was created: ${thread}`);
